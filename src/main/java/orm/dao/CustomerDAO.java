ae5119c687a0c43de9daf112291ed933cfade05f
abhishek <abhishek.solanki1110@gmail.com>
2022-01-31 00:36:33 +0530

    initial commit

---
+++ src/main/java/orm/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package orm.dao;
+
+import java.util.List;
+
+import orm.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
